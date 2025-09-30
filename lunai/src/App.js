ce4fcfe5c345f1a229b1c02629a367ce33dd7206
LucasCh12 <lucaschiapella123@gmail.com>
2025-09-30 15:57:51 -0300

    Inicio de proyecto

--- lunai/src/App.js
+++ lunai/src/App.js
@@ -4,20 +4,45 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="barra">
+        <div class="izquierda">
+            <img src="/logo.png" alt="Logo Lunai"/>
+        </div>
+    </div>
+    <div class="main-content">
+        <div class ="main-right">
+            <div class="main-column">
+                <div class="balance-section">
+                    <p class ="titleB"> Tu Dinero: </p>
+                    <p class="balance">$</p>
+                    <div class="action-buttons">
+                        <button class="deposit-btn" onclick="location.href='/not_yet'">Ingresar Dinero</button>
+                        <button class="transfer-btn" onclick="location.href='/transfer'">Transferir Dinero</button>
+                    </div>
+                </div>
+                <div class="features-section">
+                    <button class="feature-button" onclick="location.href='/not_yet'">
+                    Pagar servicios
+                    </button>
+                    <button class="feature-button" onclick="location.href='/not_yet'">
+                    Pedir Préstamo
+                    </button>
+                    <button class="feature-button" onclick="location.href='/roulette'">
+                    Gira y Gana
+                    </button>
+                    <button class="feature-button" onclick="location.href='/not_yet'">
+                    Agregar amigos
+                    </button>
+                    <button class="feature-button" onclick="location.href='/not_yet'">
+                    Compra Cripto
+                    </button>
+                    <button class="feature-button" onclick="location.href='/not_yet'">
+                    Compra Dólares
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
